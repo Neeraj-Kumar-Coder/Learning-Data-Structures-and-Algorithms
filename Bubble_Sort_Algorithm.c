cb3bd7c7fa19bcb09c31fce081d154d179038aea
Neeraj Kumar <nk7075034@gmail.com>
2021-04-30 15:02:48 +0530

    Adaptive Bubble Sort

--- Bubble_Sort_Algorithm.c
+++ Bubble_Sort_Algorithm.c
@@ -15,6 +15,7 @@ void print_array(int *array, int size);
 int main(void)
 {
     int array[] = {87, 76, 67, 54, 54, 34, 23, 21, 7, 5, 4, 4, 3, 2};
+    // int array[] = {2, 3, 4, 4, 5, 7, 21, 23, 34, 54, 54, 67, 76, 87};
     int size = sizeof(array) / sizeof(int);
     bubbleSort(array, size);
     printf("Sorted array: ");
@@ -24,8 +25,10 @@ int main(void)
 
 void bubbleSort(int arr[], int size)
 {
+    int isSorted;                      // To make it adaptive
     for (int i = 0; i < size - 1; i++) // For number of passes
     {
+        isSorted = 1;                          // To make it adaptive
         for (int j = 0; j < size - i - 1; j++) // For number of comparisions
         {
             if (arr[j] > arr[j + 1])
@@ -33,7 +36,12 @@ void bubbleSort(int arr[], int size)
                 arr[j] = arr[j] + arr[j + 1];
                 arr[j + 1] = arr[j] - arr[j + 1];
                 arr[j] = arr[j] - arr[j + 1];
+                isSorted = 0; // To make it adaptive
             }
+        }
+        if (isSorted) // To make it adaptive
+        {
+            return;
         }
     }
 }
